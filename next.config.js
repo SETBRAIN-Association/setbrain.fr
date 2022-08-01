5debb2ac41cf8819a6532394b40a9871ae51d6b8
xmanu91 <xmanu.linguet@gmail.com>
2022-08-01 19:44:40 +0200

    update

--- next.config.js
+++ next.config.js
@@ -10,4 +10,9 @@ module.exports = {
   images: {
     domains: ['res.cloudinary.com'],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
