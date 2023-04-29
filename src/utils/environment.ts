d6272ef6b8ee08881cea00b5fc6699cbc675d743
Emmanuel LINGUET <83127759+Linguet-Emmanuel@users.noreply.github.com>
2023-04-29 19:47:51 +0000

    fix: adding http before url

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -1,3 +1,8 @@
 export function getEnvironmentURI() {
-    return process.env.NEXT_PUBLIC_VERCEL_URL || process.env.NEXT_PUBLIC_LOCAL_URL || 'http://localhost:3000';
+    const url =  process.env.NEXT_PUBLIC_VERCEL_URL || process.env.NEXT_PUBLIC_LOCAL_URL || 'http://localhost:3000';
+    if (!url.includes('http')) {
+        return `https://${url}`;
+    }
+
+    return url;
 }
